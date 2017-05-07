0e6cd412512a6e5a11e5ed1f98a247222941d990
unknown <singh.vaibhav.ram@gmail.com>
2017-05-08 02:47:14 +0530

    Git error fixed

---
+++ Friendsbook/src/main/java/ando/com/loginasssignment/Helper.java
@@ -0,0 +1,14 @@
+package ando.com.loginasssignment;
+
+import android.util.Log;
+
+
+
+public class Helper {
+
+    public static User currentUser;
+
+    public static void log(String msg){
+        Log.e("RamDebugger",msg);
+    }
+}
